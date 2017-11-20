982fb804593057e684b14c8572017176d7caa445
pnghai <pnghai@gmail.com>
2017-11-20 09:46:11 +0700

    fix mix path

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,6 +8,6 @@
       }
     }
 </script>
-<script src="{{ asset('js/manifest.js') }}"></script>
-<script src="{{ asset('js/vendor.js') }}"></script>
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ mix('js/manifest.js') }}"></script>
+<script src="{{ mix('js/vendor.js') }}"></script>
+<script src="{{ mix('js/app.js') }}"></script>
